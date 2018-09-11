074aa0b5a716f538fe9f9a9a0b30472398a47d4b
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2018-09-11 00:06:25 -0400

    Use 6th degree polynomial

--- lib/temp.cpp
+++ lib/temp.cpp
@@ -9,6 +9,7 @@
 
 #include <algorithm>
 #include <cmath>
+#include <initializer_list>
 
 ////////////////////////////////////////////////////////////////////////////////
 namespace gadget
@@ -18,52 +19,74 @@ namespace gadget
 namespace
 {
 
-template<typename T>
-inline T clamp(T value, T min, T max)
+using coef = std::initializer_list<double>;
+
+coef red_2 =
 {
-    return std::max(std::min(value, max), min);
+    +1.42009689606e-14, -2.20908141363e-11, +1.39750287143e-08,
+    -4.60735885134e-06, +0.000839423340225, -0.0814873134198,
+    3.77521500948
+};
+
+coef green_1 =
+{
+    -9.92239350025e-11, +2.20158956851e-08, -1.87567079831e-06,
+    +7.58616833292e-05, -0.00157440006442, +0.03711432675,
+    -0.230587787182
+};
+
+coef green_2 =
+{
+    +7.16248383464e-15, -1.12207777143e-11, +7.16648450101e-09,
+    -2.39473016612e-06, +0.000445223682939, -0.0446935952643,
+    +2.5113795971
+};
+
+coef blue_1 =
+{
+    -1.85758346213e-11, +4.76277142815e-09, -4.24417528381e-07,
+    +8.75163420451e-06, +0.000829400768745, -0.025105117995,
+    +0.160337726094
+};
+
+double calc(coef c, double x)
+{
+    double y = 0;
+    for(auto ci = c.begin(), ce = c.end(); ci != ce; ++ci)
+        y += *ci * std::pow(x, ce - ci - 1);
+    return y;
+}
+
+inline percent clamp(percent pc)
+{
+    return std::max(std::min(pc, 100_pc), 0_pc);
 }
 
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-gadget::color to_color(gadget::temp temp)
+color to_color(temp t)
 {
-    // algorithm based on:
-    // https://github.com/neilbartlett/color-temperature
-
-    temp = clamp(temp, 100_K, 40000_K);
-
-    gadget::color color;
-    if(temp < 6600_K)
+    if(t < 6600_K)
     {
-        color.red = 100_pc;
-
-        auto green = temp / 100 - 2;
-        green = -60.88425710866345 - 0.17489000184149 * green + 40.97731842899564 * std::log(green);
-        color.green = clamp(std::round(green), 0_pc, 100_pc);
-
-        if(temp > 2000_K)
+        if(t < 400_K) t = 400_K;
+        return
         {
-            auto blue = temp / 100 - 10;
-            blue = -99.9095497416506 + 0.32447435545127 * blue + 45.3646839257496 * std::log(blue);
-            color.blue = clamp(std::round(blue), 0_pc, 100_pc);
-        }
-        else color.blue = 0_pc;
+            100_pc,
+            clamp( calc(green_1, t/100) * 100_pc ),
+            t < 2000_K ? 0_pc : clamp( calc(blue_1, t/100) * 100_pc )
+        };
     }
     else
     {
-        auto red = temp / 100 - 55;
-        red = 138.03015908551252 + 0.04478684462124 * red - 15.78575023267501 * std::log(red);
-        color.red = clamp(std::round(red), 0_pc, 100_pc);
-
-        auto green = temp / 100 - 50;
-        green = 127.62722061615584 + 0.0311508099477 * green - 11.01384170619439 * std::log(green);
-        color.green = clamp(std::round(green), 0_pc, 100_pc);
-
-        color.blue = 100_pc;
+        if(t > 40000_K) t = 40000_K;
+        return
+        {
+            clamp( calc(red_2, t/100) * 100_pc ),
+            clamp( calc(green_2, t/100) * 100_pc ),
+            100_pc
+        };
     }
-    return color;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
